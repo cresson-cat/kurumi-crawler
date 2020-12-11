ad2087c55273d32b699affe0c5cda89917a3c073
D--A--I <kanegadai@kanegadainoMacBook-Pro.local>
2020-12-11 09:44:10 +0900

    feat: コンテナ化を想定してログを出力しないようにする

--- src/helper/trailer.ts
+++ src/helper/trailer.ts
@@ -1,4 +1,5 @@
 import { SimpleLog } from './types';
+import moment from 'moment';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const baseLog: SimpleLog = require('./logger');
@@ -7,12 +8,12 @@ const baseLog: SimpleLog = require('./logger');
 type Curry1st = (prefix: string) => (message: string) => void;
 type Curry2nd = (message: string) => void;
 
-// ロガー
+// ロガー（コンテナ対応予定のため、ログファイルは未作成にする 2020/12/11）
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const _func = (writeLog: SimpleLog): Curry1st => (prefix: string): Curry2nd => (
   message: string
 ): void => {
-  console.log(message);
-  writeLog(prefix, message);
+  console.log(`${moment().format('YYYY/MM/DD HH:mm:ss')} ${message}`);
 };
 
 /* カリー化された関数（以下）を順番に返す
